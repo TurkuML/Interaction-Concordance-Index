8dd3a65f45e3107fb7f0172fe4bea2d0169ea378
Riikka Numminen <rimanu@utu.fi>
2025-08-12 12:10:02 +0300

    Modified the example to contain only one column of predictions.

--- example.py
+++ example.py
@@ -8,8 +8,9 @@ example_cols = np.array(list(range(7))*5)
 # Generate random labels within range [0.0, 1.0).
 example_Y = np.random.rand(35)
 # Generate 4 columns of random predictions within range [0.0, 1.0) to demonstrate the use with predictions made by several "hypotheses"
-# and a column of constant predictions to see that it really produces the expected random IC-index. 
-example_P = np.hstack((np.random.rand(35, 4), np.ones((35,1))))
+# and a column of constant predictions to see that it really produces the expected IC-index. 
+# example_P = np.hstack((np.random.rand(35, 4), np.ones((35,1))))
+example_P = np.ones((35,1))
 
 # Calculate the IC-indices at once for all the different "hypotheses".
 IC_indices = InteractionConcordanceIndex(example_rows, example_cols, example_Y, example_P)

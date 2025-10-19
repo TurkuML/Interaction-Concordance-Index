8a93aecbb0374181013af32da7d730488f664955
Tapio Pahikkala <tapio.pahikkala@utu.fi>
2025-10-20 01:44:04 +0300

    Update swapped.pyx

--- src/ic_index/swapped.pyx
+++ src/ic_index/swapped.pyx
@@ -24,7 +24,7 @@
 # THE SOFTWARE.
 
 from libc.stdlib cimport malloc, free
-#import numpy as np
+import numpy as np
 cimport numpy as cnp
 
 # using np.isclose, or math.isclose in this cython code affects performance considerably

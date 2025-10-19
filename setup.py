80fc2a7792198253978df662b9c2db640346a50a
Tapio Pahikkala <tapio.pahikkala@utu.fi>
2025-10-19 21:53:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules = cythonize([Extension("ic_index", ["src/ic_index/ic_index.pyx", "src/ic_index/swapped.pyx"])]),
+    ext_modules = cythonize([Extension("ic_index", ["src/ic_index/ic_index.pyx"])]),
     include_dirs=[numpy.get_include()]
 )

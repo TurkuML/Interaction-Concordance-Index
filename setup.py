a84aa346a9361300bd1c3eded1e8ddbd3aa127ba
Tapio Pahikkala <tapio.pahikkala@utu.fi>
2025-10-20 00:17:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ import numpy
 setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    ext_modules = cythonize([Extension("ic_index.ic_index", ["src/ic_index/ic_index.pyx"], include_dirs=["src/ic_index",numpy.get_include()])]),
+    ext_modules = cythonize([Extension("ic_index.ic_index", ["src/ic_index/ic_index.pyx"], include_dirs=["src/ic_index",numpy.get_include()]),
+                            Extension("ic_index.swapped", ["src/ic_index/swapped.pyx"], include_dirs=["src/ic_index",numpy.get_include()])]),
 )

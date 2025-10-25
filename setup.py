d7facd057fe1fc7b31b7cea09d359aafb31cfbc9
Tapio Pahikkala <aatapa@utu.fi>
2025-10-25 16:08:21 +0300

    Added MANIFEST.in, fine tuning of pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy
 
+extensions = [Extension("ic_index.ic_index", ["src/ic_index/ic_index.pyx"], include_dirs=["src/ic_index",numpy.get_include()])]
+
+ext_modules = cythonize(
+    extensions, 
+    compiler_directives = {'language_level' : "3"}
+)
+
 setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    ext_modules = cythonize([
-        Extension("ic_index.ic_index", ["src/ic_index/ic_index.pyx"], include_dirs=["src/ic_index",numpy.get_include()]),
-        #Extension("ic_index.swapped", ["src/ic_index/swapped.pyx"], include_dirs=["src/ic_index",numpy.get_include()])
-    ]),
+    ext_modules = ext_modules
 )
+

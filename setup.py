547dddd7f2f036443c8c1fe51588d55fb52f3a9e
Tapio Pahikkala <aatapa@utu.fi>
2025-10-30 11:25:27 +0200

    Read long description from README.md

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 extensions = [Extension("ic_index.ic_index", ["src/ic_index/ic_index.pyx"], include_dirs=["src/ic_index",numpy.get_include()])]
 
 ext_modules = cythonize(
@@ -12,6 +16,8 @@ ext_modules = cythonize(
 setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    ext_modules = ext_modules
+    ext_modules = ext_modules,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
 
